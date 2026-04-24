2d39c7ea3fa39029aadf016b2a39fe71d410bb6a
Tonii2100 <ouanthony9@gmail.com>
2026-04-23 21:16:26 -0700

    formatted spacing.

--- src/test/java/org/example/triviagame/ScoreDAOTest.java
+++ src/test/java/org/example/triviagame/ScoreDAOTest.java
@@ -1,10 +1,9 @@
 package org.example.triviagame;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 
